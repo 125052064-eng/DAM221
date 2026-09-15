02b2d3fb0f11d479923602155afefb47319a52ef
125052064-eng <125052064@upq.edu.mx>
2026-09-14 20:28:12 -0600

    practica 1, introduccion a JS

---
+++ introJS/lógica.js
@@ -0,0 +1,4 @@
+let nombre = prompt("¿Cuál es tu nombre?: ");
+        console.log("Hola " + nombre + ", bienvenido a la clase de JS");
+        document.write("<h1>Hola " + nombre + ", bienvenido a la clase de JS</h1>");
+        
